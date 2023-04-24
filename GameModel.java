e049ca3f77d3746448d7dd69be1fecb4991af8ba
asdadsdasd <mkozarez12345@mail.ru>
2023-04-24 10:49:59 +0300

    second - 4

--- GameModel.java
+++ GameModel.java
@@ -26,7 +26,7 @@ public class GameModel {
     public GameField field(){
         return this.gameField;
     }
-    
+    //second - 4
 // -------------------------------- Игроки -----------------------------------
     
     private ArrayList<Player> _playerList = new ArrayList<>();
