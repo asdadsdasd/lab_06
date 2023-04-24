04c90867f0d9a814b17c465b3c48314dd7486984
asdadsdasd <mkozarez12345@mail.ru>
2023-04-24 10:49:40 +0300

    second - 3

--- GameModel.java
+++ GameModel.java
@@ -17,7 +17,7 @@ import xzero.model.navigation.Direction;
 * целью определения конца игры
 */
 public class GameModel {
-    
+//second - 3   
 // -------------------------------- Поле -------------------------------------
     
     //!!!
